b54b76b38b239250dfbc41a5039c5aef6ba31d68
Naveen Gupta <naveen.gupta@ipro-india.com>
2023-03-07 10:41:29 +0530

    Logging and exception

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import find_packages,setup
 from typing import List
 
-
-HYPHEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->list[str]:
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return the list of requirements
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines
-        requirements=[req.replace("\n","")for req in requirements]
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
+
 setup(
-name = 'mlproject',
-version = '0.0.1',
-author = 'IPRO16',
-author_email='IPRO16@I-52.com',
+name='mlproject',
+version='0.0.1',
+author='Naveen',
+author_email='naveen.gupta@ipro-india.com',
 packages=find_packages(),
-install_requires = get_requirements('requirements.txt')
+install_requires=get_requirements('requirements.txt')
+
 )
