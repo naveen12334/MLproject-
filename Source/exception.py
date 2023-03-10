dbd8faf1d4b72cb65516a8e9be3f604bac9405fc
Naveen Gupta <naveen.gupta@ipro-india.com>
2023-03-10 15:39:00 +0530

    Data ingestion

--- Source/exception.py
+++ Source/exception.py
@@ -1,6 +1,7 @@
 
 import sys
-import logging
+#import logging
+from Source.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -20,6 +21,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
-
     
