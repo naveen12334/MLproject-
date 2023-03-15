1624b1deb69d97d0506f6df0b871b8778fb11b1d
Naveen Gupta <naveen.gupta@ipro-india.com>
2023-03-15 13:06:36 +0530

    Data Transformation is done

--- Source/components/data_transformation.py
+++ Source/components/data_transformation.py
@@ -16,7 +16,7 @@ from Source.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('articacts',"preprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
